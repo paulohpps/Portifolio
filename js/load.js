d1b56943c847f23f11bccfc99d8f898448cdde48
Paulo Henrique Pereira Pires da Silva <paulohpps13@gmail.com>
2022-01-27 15:28:37 -0300

    tela de loading adicionada

---
+++ js/load.js
@@ -0,0 +1,7 @@
+var load = document.getElementById("loading");
+function loadfun()
+{
+    load.style.visibility = "hidden"
+    load.style.opacity = 0
+    load.style.transition = "visibility 0s 1s, opacity 1s linear";
+}
