bd416e658735e50ab5a77511fe55eb57c4b18472
Paulo Henrique Pereira Pires da Silva <paulohpps13@gmail.com>
2022-01-27 10:36:01 -0300

    Codigo js adicionado

---
+++ js/escreverTexto.js
@@ -0,0 +1,19 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    for(let i = 0; i < textoArray.length; i++) {
+        setTimeout(() => elemento.innerHTML += textoArray[i], 150 * i);
+     }
+    }
+    function typeWritertexto(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    for(let i = 0; i < textoArray.length; i++) {
+        setTimeout(() => elemento.innerHTML += textoArray[i], 25 * i);
+     }
+    }
+  
+    const titulo = document.querySelector('.titulo');
+    const texto = document.querySelector('.texto')
+    typeWriter(titulo);
+    typeWritertexto(texto);
