77af15d883f09910277ea392dd5f349be7784340
Paulo Henrique Pereira Pires da Silva <paulohpps13@gmail.com>
2022-01-28 23:29:59 -0300

    porcentagem

--- js/transicao.js
+++ js/transicao.js
@@ -34,3 +34,22 @@ const debounce = function(func, wait, immediate) {
       animeScroll();
     }, 200));
   }
+  const animationClass2 = 'animates';
+  const target2 = document.querySelectorAll('[data-por]');
+  function animeScroll2() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target2.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass2);
+      } else {
+        element.classList.remove(animationClass2);
+      }
+    })
+  }
+  
+  animeScroll2();
+  if(target2.length) {
+    window.addEventListener('scroll', debounce(function() {
+      animeScroll2();
+    }, 200));
+  }
